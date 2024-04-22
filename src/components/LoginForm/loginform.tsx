7b2d70f205715fb960869ffdc97799000a9839ee
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-04-22 11:39:47 -0300

    feat: page de login estruturada

--- src/components/LoginForm/loginform.tsx
+++ src/components/LoginForm/loginform.tsx
@@ -4,6 +4,7 @@ import { Card } from '../ui/card';
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { useForm } from 'react-hook-form';
+import { Label } from '@radix-ui/react-label';
 
 interface LoginFormInputs {
     email: string;
@@ -18,30 +19,40 @@ export const LoginForm: React.FC = () => {
     };
 
     return (
-        <Card className="w-96 p-6">
-            <h2 className="text-xl font-semibold mb-4">Entre utilizando o email</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-4">
-                    <Input
-                        type="email"
-                        placeholder="Email"
-                        {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-                    />
-                    {errors.email && <p className="text-red-500">Email inválido</p>}
-                </div>
-                <div className="mb-4">
-                    <Input
-                        type="password"
-                        placeholder="Senha"
-                        {...register("password", { required: true })}
-                    />
-                    {errors.password && <p className="text-red-500">Senha obrigatória</p>}
-                </div>
-                <Button type="submit" className="w-full">
-                    Entrar
-                </Button>
-            </form>
-        </Card>
+        <div className="flex items-center justify-center min-h-screen bg-black">
+            <Card className="w-[350px] p-6 mx-auto bg-black border border-gray-800 rounded-lg space-y-4" >
+                <h2 className="text-xl font-semibold mb-4 text-white">Entre utilizando o email</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-4">
+                        <Label htmlFor='email' className="text-white">E-mail</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="Email"
+                            className="bg-black border border-gray-800 text-white"
+                            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+                        />
+                        {errors.email && <p className="text-red-500">Email inválido</p>}
+                    </div>
+                    <div className="mb-4">
+                        <Label htmlFor='senha' className="text-white">Senha</Label>
+                        <Input
+                            id="senha"
+                            type="password"
+                            placeholder="Senha"
+                            className="bg-black border border-gray-800 text-white"
+                            {...register("password", { required: true })}
+                        />
+                        {errors.password && <p className="text-red-500">Senha obrigatória</p>}
+                    </div>
+                    <Button type="submit" className="w-full" variant="secondary">
+                        Entrar
+                    </Button>
+                </form>
+            </Card>
+        </div>
     );
 };
+
+
 
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,20 @@
 import { LoginForm } from "@/components/LoginForm/loginform";
+import { Button } from '../components/ui/button';
+import Link from 'next/link';
+import "../app/globals.css"
 
 export default function Login() {
     return (
-        <>
+        <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden transition-all ease-in-out duration-500">
+            <div className="absolute top-4 left-4 transition-all ease-in-out duration-500">
+                <h1 className="text-2xl font-bold text-white">Young Dream Data 📚</h1>
+            </div>
+            <div className="absolute top-4 right-4 transition-all ease-in-out duration-500">
+                <Link href="/" passHref>
+                    <Button variant="secondary">Voltar</Button>
+                </Link>
+            </div>
             <LoginForm />
-        </>
-    )
+        </div>
+    );
 }
