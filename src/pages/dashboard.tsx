cc74664747ed5e7b4bd7aa79bc781274b5fa2093
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-04-27 10:34:54 -0300

    iniciando estruturação da dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,21 @@
+import "../app/globals.css"
+import Link from 'next/link';
+import { Button } from '../components/ui/button';
+
 export default function Dashboard() {
     return (
         <>
-            <h1> oiii </h1>
+            <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden transition-all ease-in-out duration-500">
+            <div className="absolute top-4 left-4 transition-all ease-in-out duration-500">
+                <h1 className="text-2xl font-bold text-white">Young Dream Data 📚</h1>
+            </div>
+            <div className="absolute top-4 right-4 transition-all ease-in-out duration-500">
+                <Link href="/" passHref>
+                    <Button variant="secondary">Sair</Button>
+                </Link>
+                <Button variant="secondary">Cadastre um aluno</Button>
+            </div>
+            </div>
         </>
     )
 }
