0d1e8e88b993d96d0c3c3bb615deb7b761cbacc4
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-04-28 22:14:52 -0300

    feat: banco estruturado com PRISMA e DOCKER em PSQL. Rota de registro finalizada e funcional, gerando TOKEN e cadastrando o usuário no banco corretamente com Prisma

--- src/components/RegisterForm/registerform.tsx
+++ src/components/RegisterForm/registerform.tsx
@@ -5,20 +5,46 @@ import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { useForm } from 'react-hook-form';
 import { Label } from '@radix-ui/react-label';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 
 interface RegisterFormInputs {
-    name: string;
+    nome: string;
     email: string;
-    password: string;
-    institution: string;
+    senha: string;
+    instituicao: string;
 }
 
 export const RegisterForm: React.FC = () => {
     const { register, handleSubmit, formState: { errors } } = useForm<RegisterFormInputs>();
+    const router = useRouter();
 
-    const onSubmit = (data: RegisterFormInputs) => {
-        console.log('Dados do formulário:', data);
-    };
+    const onSubmit = async (data: RegisterFormInputs) => {
+        try {
+          const response = await fetch('/api/register', { 
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+          });
+    
+          if (response.ok) {
+            const result = await response.json();
+            toast.success(result.message); 
+            localStorage.setItem('token', result.token);
+            setTimeout(() => {
+                router.push('/dashboard');
+            }, 2000);
+          } else {
+            const error = await response.json();
+            toast.error(error.error); 
+          }
+        } catch (error) {
+          console.error('Registration error:', error);
+          toast.error('Ocorreu um erro durante o registro. Tente novamente.'); 
+        }
+      };
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-black">
@@ -28,13 +54,13 @@ export const RegisterForm: React.FC = () => {
                     <div className="mb-4">
                         <Label htmlFor='name' className="text-white">Nome</Label>
                         <Input
-                            id="name"
+                            id="nome"
                             type="text"
                             placeholder="Nome"
                             className="bg-black border border-gray-800 text-white"
-                            {...register("name", { required: true })}
+                            {...register("nome", { required: true })}
                         />
-                        {errors.name && <p className="text-red-500">Nome obrigatório</p>}
+                        {errors.nome && <p className="text-red-500">Nome obrigatório</p>}
                     </div>
                     <div className="mb-4">
                         <Label htmlFor='email' className="text-white">E-mail</Label>
@@ -54,20 +80,20 @@ export const RegisterForm: React.FC = () => {
                             type="password"
                             placeholder="Senha"
                             className="bg-black border border-gray-800 text-white"
-                            {...register("password", { required: true })}
+                            {...register("senha", { required: true })}
                         />
-                        {errors.password && <p className="text-red-500">Senha obrigatória</p>}
+                        {errors.senha && <p className="text-red-500">Senha obrigatória</p>}
                     </div>
                     <div className="mb-4">
-                        <Label htmlFor='institution' className="text-white">Instituição de Ensino</Label>
+                        <Label htmlFor='instituicao' className="text-white">Instituição de Ensino</Label>
                         <Input
-                            id="institution"
+                            id="instituicao"
                             type="text"
                             placeholder="Instituição de Ensino"
                             className="bg-black border border-gray-800 text-white"
-                            {...register("institution", { required: true })}
+                            {...register("instituicao", { required: true })}
                         />
-                        {errors.institution && <p className="text-red-500">Instituição de ensino obrigatória</p>}
+                        {errors.instituicao && <p className="text-red-500">Instituição de ensino obrigatória</p>}
                     </div>
                     <Button type="submit" className="w-full border border-gray-800" variant="secondary">
                         Registrar
---
+++ src/pages/api/register.ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { prisma } from '../../../database';
+
+interface RegisterBody {
+    nome: string;
+    email: string;
+    senha: string;
+    instituicao: string;
+  }
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+  ): Promise<void> {
+    const { nome, email, senha, instituicao }: RegisterBody = req.body;
+  
+    if (!nome || !email || !senha || !instituicao) {
+      return res.status(400).json({ error: 'Preencha todos os campos!' });
+    }
+  
+    const existingUser = await prisma.professor.findUnique({
+      where: { email },
+    });
+  
+    if (existingUser) {
+      return res.status(400).json({ error: 'E-mail já cadastrado!' });
+    }
+  
+    const hashedPassword = await bcrypt.hash(senha, 10);
+  
+    await prisma.professor.create({
+      data: {
+        nome,
+        email,
+        senha: hashedPassword,
+        instituicao,
+      },
+    });
+  
+    const token = jwt.sign({ email }, '97dc3f2852a79202f397d1714672f181fadf5b1a', { expiresIn: '1h' });
+  
+    res.status(201).json({ message: 'Registro realizado com sucesso!', token });
+  }
+  
